a8759feec6172f404ee2c32b2a47c32449bd4519
Rúben França <ruben_tiagof@hotmail.com>
2020-12-11 12:17:10 +0000

    Add header component

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
 
 const App = () => {
   return (
-    <div>
-      <h1>Hi There!!!</h1>
-      <hr />
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   );
 };
 
