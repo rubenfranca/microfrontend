4874f840144f747a70d41be98b075d78c6020a03
Rúben França <ruben_tiagof@hotmail.com>
2020-12-11 16:51:18 +0000

    Add auth logic

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -11,14 +11,18 @@ const generateClassName = createGenerateClassName({
   productionPrefix: 'au',
 });
 
-const App = ({ history }) => {
+const App = ({ history, onSignIn }) => {
   return (
     <div>
       <StylesProvider generateClassName={generateClassName}>
         <Router history={history}>
           <Switch>
-            <Route path='/auth/signin' component={Signin} />
-            <Route path='/auth/signup' component={Signup} />
+            <Route path='/auth/signin'>
+              <Signin onSignIn={onSignIn} />
+            </Route>
+            <Route path='/auth/signup'>
+              <Signup onSignIn={onSignIn} />
+            </Route>
           </Switch>
         </Router>
       </StylesProvider>
--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense, useState } from 'react';
 import {
   StylesProvider,
   createGenerateClassName,
@@ -15,14 +15,21 @@ const generateClassName = createGenerateClassName({
 });
 
 const App = () => {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
   return (
     <BrowserRouter>
       <StylesProvider generateClassName={generateClassName}>
         <div>
-          <Header />
+          <Header
+            isSignedIn={isSignedIn}
+            onSignOut={() => setIsSignedIn(false)}
+          />
           <Suspense fallback={<Progress />}>
             <Switch>
-              <Route path='/auth' component={AuthLazy} />
+              <Route path='/auth'>
+                <AuthLazy onSignIn={() => setIsSignedIn(true)} />
+              </Route>
               <Route path='/' component={MarketingLazy} />
             </Switch>
           </Suspense>
