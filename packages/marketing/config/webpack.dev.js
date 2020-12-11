a91e380d4e1bd91a7a327bed9c24e70e201dcff6
Rúben França <ruben_tiagof@hotmail.com>
2020-12-11 02:25:04 +0000

    Created container deploy script

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -1,11 +1,10 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
 const devConfig = {
-  mode: "development",
+  mode: 'development',
   devServer: {
     port: 8081,
     historyApiFallback: {
@@ -20,9 +19,6 @@ const devConfig = {
         './MarketingApp': './src/bootstrap',
       },
       shared: packageJson.dependencies,
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
     }),
   ],
 };
